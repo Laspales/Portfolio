e0675cb4706593b6a9cc0e19aac66b0c4bf9b230
Laspales <badepalla09@gmail.com>
2024-08-28 06:15:59 +0200

    update_cv_and_profile_pic

--- Portfolio/resources/views/aboutme.blade.php
+++ Portfolio/resources/views/aboutme.blade.php
@@ -3,7 +3,7 @@
 @section('content')   
     <div class="texte">
         <div id="img">
-            <img src="/images/bad_epalla.png"/><br/><br/>
+            <img src="/images/profile_pic/lasputepp01.png"/><br/><br/>
         </div>
         <p> I'm <span>Epalla Ndjalla Laspales</span>, a student from Cameroon. Currently,
             I'm continuing my studies at ETNA in Ivry-Sur-Seine, where I'm registered as a
@@ -13,7 +13,7 @@
            <br/>
             Outside of my studies, my passions include football, cycling, badminton, reading, and music.
             To find out more about my academic background and my skills, do not hesitate to consult
-            <a href="/pdf/CV_EPALLA_LASPALES_ETNA.pdf" target="_blank">My CV</a>.
+            <a href="/pdf/CV_Laspales_Epalla .pdf" target="_blank">My CV</a>.
          </p>
     </div>   
 @endsection
